1ab9794513ce94a602e3d07c2c1abb100d8ae0bd
Joris <j.fabre@rubika-edu.com>
2018-12-04 15:31:29 +0100

    Obtention Lamp Scene
    Vous êtes tous très beau!

---
+++ Assets/RevealAfterLamp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevealAfterLamp : MonoBehaviour {
+
+    public static RevealAfterLamp staticReveal;
+    public GameObject grandPont;
+    public GameObject salleTransfomée;
+    public GameObject player;
+
+    private void Start()
+    {
+        staticReveal = this;
+    }
+
+    private void FixedUpdate()
+    {
+        if(player.GetComponent<Player>().HasLamp == true)
+        {
+            Debug.Log("Choppe la lampe");
+            grandPont.SetActive(false);
+            salleTransfomée.SetActive(true);
+        }
+    }
+
+
+}
