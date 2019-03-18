15039f4a61c11c47583b1a0f9d8b967a7bfe8926
Demotion <e4axel@gmail.com>
2019-03-18 12:29:01 +0100

    Kaleidoscope Lights
    To add Lore later on

---
+++ Assets/LightTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTrigger : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("LightOn");
+        Debug.Log("Entered Light Trigger " + gameObject.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetTrigger("LightOff");
+        Debug.Log("Exited Light Trigger " + gameObject.name);
+    }
+}
